4673916e6e7db23dbe53a0e23b419a098291a9dc
Manuel Alvarado <manuel.alvarado@vcaperu.com>
2019-07-12 11:01:04 -0500

    server added

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('./middleware/cors');
+//Init App
+const PORT = process.env.PORT || 3000;
+const app = express();
+//Middleware
+app.use(cors.allowCORS);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
+//Routes
+const pokemonRouter = express.Router();
+pokemonRouter.get('/',(req,res) => {
+    res.status(200).send("Pokemon list");
+});
+app.use('/api/pokemon',pokemonRouter);
+//Create server and listen
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
